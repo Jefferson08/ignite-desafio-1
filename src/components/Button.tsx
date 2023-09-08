46648981de4c368eac6e473301f97e5aa5e2b635
jefferson08 <jeffersoncarvalho60@outlook.com>
2023-09-07 22:15:03 -0300

    Component Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,23 @@
+import {ButtonHTMLAttributes} from "react";
+import Icon from "phosphor-react";
 
-interface ButtonProps {
-    text: string
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+    text: string,
+    icon?: Icon.IconProps
 }
-export const Button = (props: ButtonProps) => {
-
-    const {text} = props;
-
+export const Button = ({text, icon, ...props}: ButtonProps) => {
     return (
-        <button>
+        <button
+            className="p-4 text-sm leading-5 font-bold text-center inline-flex items-center gap-2 text-white bg-blue-dark rounded-lg hover:bg-blue focus:none focus:outline-none"
+            {...props}
+        >
             {text}
+
+            {icon && (
+                <>
+                    {icon}
+                </>
+            )}
         </button>
     )
 }
